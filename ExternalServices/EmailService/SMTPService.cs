defa174304a796900ec6ae98f8cc8fd7555dd241
felipeayalmeida <feay.almeida@gmail.com>
2024-11-07 20:51:02 -0300

    initiating email service

---
+++ ExternalServices/EmailService/SMTPService.cs
@@ -0,0 +1,26 @@
+﻿using StockPriceNotificator.Models;
+using System.Net.Mail;
+using System.Net;
+
+namespace StockPriceNotificator.ExternalServices.EmailService
+{
+    public class SMTPService
+    {
+        private readonly ConfigurationModel _config;
+
+        public SMTPService(ConfigurationModel config)
+        {
+            _config = config;
+        }
+        public void SendEmail()
+        {
+            using var smtpClient = new SmtpClient(_config.SMTPServer, _config.SMTPPort)
+            {
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                EnableSsl = true,
+                Credentials = new NetworkCredential(_config.SMTPUsername, _config.SMTPPassword),
+            };
+        }
+    }
+}
