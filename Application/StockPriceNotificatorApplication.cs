77d1db970825fada4cbebf9b2f87b9ba53f22b95
felipeayalmeida <feay.almeida@gmail.com>
2024-11-06 21:24:30 -0300

    folders structures and application first code

---
+++ Application/StockPriceNotificatorApplication.cs
@@ -0,0 +1,26 @@
+﻿namespace StockPriceNotificator.Application
+{
+    public class StockPriceNotificatorApplication
+    {
+        private readonly string _asset;
+        private readonly decimal _sellPrice;
+        private readonly decimal _buyPrice;
+
+        public StockPriceNotificatorApplication(string asset, decimal sellPrice, decimal buyPrice)
+        {
+            _asset = asset;
+            _sellPrice = sellPrice;
+            _buyPrice = buyPrice;
+        }
+
+        public async Task StartMonitoringAsync()
+        {
+            Console.WriteLine($"Monitoring {_asset}, with sellPrice = {_sellPrice} and buyPrice = {_buyPrice}...");
+            while (true)
+            {
+                Console.WriteLine("Do External Calls");
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}
